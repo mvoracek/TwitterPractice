79cdecc9c3187a64ba985b94d4372a552d7cea25
mjvoracek <mjvoracek@gmail.com>
2014-12-01 10:03:44 -0600

    Initial Commit

---
+++ TwitterPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterPractice
+//
+//  Created by Matthew Voracek on 12/1/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwitterPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterPractice
+//
+//  Created by Matthew Voracek on 12/1/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
