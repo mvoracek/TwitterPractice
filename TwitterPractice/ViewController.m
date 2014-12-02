e4eed475a0d517d01b1e6b69266361f281950663
mjvoracek <mjvoracek@gmail.com>
2014-12-02 08:53:24 -0600

    initial commit

--- TwitterPractice/ViewController.m
+++ TwitterPractice/ViewController.m
@@ -7,21 +7,58 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *searchTextField;
+
+@property (nonatomic, strong) NSURLSession *session;
 
 @end
 
+static NSString *TWITTER_CONSUMER_KEY = @"fx95oKhMHYgytSBmiAqQ";
+static NSString *TWITTER_CONSUMER_SEC = @"0zfaijLMWMYTwVosdqFTL3k58JhRjZNxd2q0i9cltls";
+static NSString *OAUTH_TOKEN = @"2305278770-GGw8dQQg3o5Vqfx9xHpUgJ0CDUe3BoNmUNeWZBg";
+static NSString *OAUTH_SECRET = @"iEzxeJjEPnyODVcoDYt5MVvrg90Jx2TOetGdNeol6PeYp";
+
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    self.session = [NSURLSession sessionWithConfiguration:config];
+    NSURL *searchURL = [self createURLWithSearch];
+    [self searchResultsFromURL:searchURL];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSURL *)createURLWithSearch
+{
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/search/tweets.json?q=wwdc"];
+    return url;
+}
+
+- (void)searchResultsFromURL:(NSURL *)url
+{
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+            NSLog(@"%ld", (long)httpResp.statusCode);
+            if (httpResp.statusCode == 200) {
+                NSDictionary *searchJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+                NSLog(@"%@", searchJSON);
+                
+            } else {
+                //json error handling
+            }
+        } else {
+            //error handling
+        }
+    }];
+    [dataTask resume];
 }
 
 @end
